71a80e86125a426ba8f633d45b9ca943a4a5a068
intent-ly <erick.skillsusa@gmail.com>
2020-09-08 15:31:39 -0700

    Java-Script Prompts

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
 /************************************************************** Task 1: Warm-up! **************************************************************/
 //Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
 
+var votingAge =21;
 
-
-
+if(votingAge>18){
+    console.log("TRUE")
+}
 
 //Task b: declare a variable and then use a conditional to change the value of that variable based on the value assigned to a second variable (no function required)
 
+var intA = 1;
+var intB = 2;
 
-
+if(intB===2)
+{
+    intA=3;
+    console.log(intA)
+}
 
 
 //Task c: Convert string ("1999") to integer (1999)  (no function required) // hint look up the Number method
 
-
-
-
+ Number("1999")
 
 //Task d: Write a function to multiply a*b 
 
-
+function multiplicationFunction(a,b){
+    return a*b;
+}
 
 
 
@@ -27,9 +35,11 @@
 //Age in Dog years
 //write a function that takes your age and returns it to you in dog years - they say that 1 human year is equal to seven dog years 
 
+function dogYears(a){
+    return a * 7;
+}
 
-
-
+console.log(dogYears(20));
 
 /************************************************************** Task 3 **************************************************************/
 //Dog feeder 
@@ -49,9 +59,36 @@
 
 // when you are finished invoke your function with the weight of 15 lbs and the age of 1 year - if your calculations are correct your result should be 0.44999999999999996
   
+function dogFeeder(age, weight){
+    if(age >= 1){
+        if(weight <=5)
+        {
+            return .05 * weight;
+        }
+        else if(weight>5 && weight<=10){
+            return .04 * weight;
+        }
+        else if(weight>10 && weight<=15){
+            return .03 * weight;
+        }
+        else if(weight>15){
+            return .02 * weight;
+        }
+    }
+    else if(age < 1){
+        if(age>=.1667 && age<.3333){
+            return .1 * weight;
+        }
+        else if(age>=.3333 && age<.5833){
+            return .05 * weight;
+        }
+        else if(age>=.5833 && age<1 ){
+            return .04 * weight;
+        }
+    }
+}
 
-
-
+console.log(dogFeeder(1,15));
 
 /************************************************************** Task 4 **************************************************************/
 // Rock, Paper, Sissors
@@ -60,20 +97,79 @@
 // use math.random to determine the computers choice 
 // hint while you can complete this with only conditionals based on strings it may help to equate choice to a number 
 
-  
+const getUserChoice = userInput => {
+    if(userInput === 'rock'||  userInput === 'paper' || userInput === 'scissors'){
+        return userInput
+    } else{
+        console.log('Error!')
+    }
+}
+
+const getComputerChoice = () => {
+    const randomNumber = Math.floor(Math.random() * 3);
+    if(randomNumber === 0){
+        return('rock');
+    }else if(randomNumber === 1){
+        return('paper');
+    }else{
+        return('scissors')
+    }
+};
+
+const determineWinner = (userChoice, computerChoice) => {
+    if(userChoice === computerChoice) {
+        return 'The game is a tie!'
+    };
+    if(userChoice === 'rock'){
+        if(computerChoice === 'paper'){
+            return 'The computer won!'
+        } else{
+            return 'You won!'
+        }
+    }
+    if(userChoice === 'paper'){
+        if(computerChoice === 'rock'){
+            return 'You won!'
+        } else{
+            return 'The computer won!'
+        }
+    }
+    if(userChoice === 'scissors'){
+        if(computerChoice === 'paper'){
+            return 'The computer won!'
+        } else{
+            return 'You won!'
+        }
+    }
+};
+
+const playGame =() => {
+    const userChoice = getUserChoice('paper');
+    const computerChoice = getComputerChoice();
+    console.log('You chose: ' + userChoice);
+    console.log('The computer chose: ' + computerChoice);
+
+    console.log(determineWinner(userChoice, computerChoice));
+};
+
+playGame()
+
   
 
 /************************************************************** Task 5 **************************************************************/
 //Metric Converter
 //a. KM to Miles - should take the number of kilometers and convert it to the equal number of miles
 
-
-
+function kmMiles(kmLength){
+    return kmLength * 0.62137
+}
 
 
 //b. Feet to CM - should take the number of feet and convert it to the equal number of centimeters
   
-
+function feetCM(feetLength){
+    return feetLength/0.032808;
+}
 
 
 
@@ -82,9 +178,15 @@
 // create a function called annoyingSong
 // the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
   
-
-
-
+function annoyingSong(){
+    i=99;
+    while(i>1){
+        console.log(i); console.log("bottles of soda on the wall, ");
+        console.log(i); console.log(" bottles of soda, take one down, pass it around ");
+        console.log(i-1); console.log("bottles of soda on the wall.");
+        i=i-1;
+    }
+}
 
 /************************************************************** Task 7 **************************************************************/
 //Grade Calculator
@@ -95,7 +197,23 @@
 //60s should be D 
 //and anything below 60 should be F
   
-
+function letterGrade(grade){
+    if(grade>= 90){
+        console.log("A");
+    }
+    else if(grade <90 && grade>=80){
+        console.log("B");
+    }
+    else if(grade<80 && grade>=70){
+        console.log("C");
+    }
+    else if(grade<70 && grade>=60){
+        console.log("D");
+    }
+    else if(grade<60){
+        console.log("F");
+    }
+}
   
   
 
